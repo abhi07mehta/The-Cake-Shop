46020b176d02fc1309e4b9998f2d21c59a9105ba
Abhishek Mehta <65299223+abhi07mehta@users.noreply.github.com>
2024-07-27 18:31:32 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,6 @@ import { getFirestore } from "firebase/firestore";
 import {getStorage} from "firebase/storage"
 import "firebase/firestore";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyB7nvIEvG_YBpc6RK5EDYdYkO2dlwJ-Oag",
-    authDomain: "the-cake-shop-be95d.firebaseapp.com",
-    projectId: "the-cake-shop-be95d",
-    storageBucket: "the-cake-shop-be95d.appspot.com",
-    messagingSenderId: "825073331662",
-    appId: "1:825073331662:web:d88c74853f2a7b0d46cf9f",
-    measurementId: "G-KJXWSNJJYZ"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
